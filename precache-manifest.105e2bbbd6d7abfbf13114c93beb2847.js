2f6267c5a4c9531ae6d9219c9f08d8ebddbca84c
Prathmesh Santosh Dhatrak <prathmesh101dhatrak@gmail.com>
2020-08-14 09:56:32 +0530

    Updates

---
+++ precache-manifest.105e2bbbd6d7abfbf13114c93beb2847.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8d9afa60de312735ca66a8da92919a",
+    "url": "/breaking-bad-web/index.html"
+  },
+  {
+    "revision": "5e89d99e07d79da99f4d",
+    "url": "/breaking-bad-web/static/css/main.6d3e611a.chunk.css"
+  },
+  {
+    "revision": "2a9d6f2d4eccc0b113ee",
+    "url": "/breaking-bad-web/static/js/2.08933941.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-web/static/js/2.08933941.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e89d99e07d79da99f4d",
+    "url": "/breaking-bad-web/static/js/main.7857228f.chunk.js"
+  },
+  {
+    "revision": "141c32b3ffb157c15b8e",
+    "url": "/breaking-bad-web/static/js/runtime-main.e882a2ae.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-web/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-web/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-web/static/media/spinner.235b9b3e.gif"
+  }
+]);
